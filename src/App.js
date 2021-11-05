fcffe1baf80200e8f7d7340fc2be491480636c01
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-11-05 17:04:15 +0600

    Header adder, routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./custom.css";
+import Home from "./Pages/Home/Home";
+import Services from "./Pages/Services/Services";
+import Header from "./Shared/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Let's begin</h2>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Shared/Header.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+//import useAuth from "../Hooks/useAuth";
+
+const Header = () => {
+  const activeStyle = {
+    color: "white",
+    textDecoration: "none",
+  };
+  // const { user, logOut } = useAuth();
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        sticky="top"
+      >
+        <Container>
+          <Navbar.Brand>
+            <img
+              alt=""
+              src="./logo.png"
+              width="70"
+              height="70"
+              className="d-inline-block "
+            />{" "}
+            <NavLink to="/home" style={activeStyle}>
+              Travel Planner EU
+            </NavLink>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav.Link style={activeStyle} as={Link} to="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link style={activeStyle} as={Link} to="/services">
+              Services
+            </Nav.Link>
+            <Nav.Link style={activeStyle} as={Link} to="/doctors">
+              Find A Doctor
+            </Nav.Link>
+            <Nav.Link style={activeStyle} as={Link} to="/faq">
+              FAQ
+            </Nav.Link>
+            {/* {user?.email ? (
+              <Navbar.Text>
+                <span style={activeStyle}>
+                  Signed in as: {user?.displayName}
+                </span>
+                <Button variant="light" onClick={logOut} className="ms-3">
+                  Sign Out
+                </Button>
+              </Navbar.Text>
+            ) : (
+              <Nav.Link style={activeStyle} as={Link} to="/login">
+                Login
+              </Nav.Link>
+            )} */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
