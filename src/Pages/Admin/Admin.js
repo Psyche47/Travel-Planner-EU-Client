98be7d3be77f55ec4874a4c046368cf01ff509d5
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-11-06 14:38:01 +0600

    Dashboard Polished

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,28 +1,45 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, ListGroup, Row } from "react-bootstrap";
 
 const Admin = () => {
+  const [control, setControl] = useState("addServices");
+  console.log(control);
   return (
     <div>
       <h2 className="text-info text-center mt-1 bg-dark rounded mx-2 p-2">
         Admin Panel
       </h2>
-
-      <Container>
-        <Row>
-          <Col className="border border-dark p-1 bg-dark" lg={3} md={3} sm={12}>
-            <h4 className="text-center text-light">Dashboard</h4>
-            <ul className="text-light">
-              <li>Add A Service</li>
-              <li>Manage All Services</li>
-              <li>Logout</li>
-            </ul>
-          </Col>
-          <Col lg={9} md={9} sm={12}>
-            <h2>Hiiii</h2>
-          </Col>
-        </Row>
-      </Container>
+      <Row className="mx-2 border rounded ">
+        <Col className="border border-dark p-1 bg-dark" lg={3} md={3} sm={12}>
+          <h4 className="text-center text-light">Dashboard</h4>
+          <ListGroup as="ol" numbered>
+            <ListGroup.Item variant="dark" as="li" className="admin-item">
+              <Button
+                variant="success"
+                onClick={() => setControl("addServices")}
+              >
+                {" "}
+                Add A Service
+              </Button>
+            </ListGroup.Item>
+            <ListGroup.Item variant="dark" as="li" className="admin-item">
+              <Button
+                variant="success"
+                onClick={() => setControl("manageServices")}
+              >
+                Manage All Services
+              </Button>
+            </ListGroup.Item>
+            <ListGroup.Item variant="dark" as="li" className="admin-item">
+              <Button variant="danger" onClick={() => setControl("logOut")}>
+                {" "}
+                Logout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col lg={9} md={9} sm={12}></Col>
+      </Row>
     </div>
   );
 };
