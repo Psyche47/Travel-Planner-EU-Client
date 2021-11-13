bba0e453ca4b51e5aba3714a702905f947e93ebd
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-11-14 04:22:15 +0600

    Updated My bookings

--- src/Hooks/useBookings.js
+++ src/Hooks/useBookings.js
@@ -4,12 +4,13 @@ import useAuth from "./useAuth";
 const useBookings = () => {
   const { user } = useAuth();
   const [bookings, setBookings] = useState([]);
+  const [control, setControl] = useState(false);
   useEffect(() => {
     fetch(`http://localhost:5000/myBookings/${user.email}`)
       .then((res) => res.json())
       .then((data) => setBookings(data));
-  }, []);
-  return [bookings, setBookings];
+  }, [control]);
+  return [bookings, setControl, control];
 };
 
 export default useBookings;
--- src/Pages/MyBookings/MyBookings.js
+++ src/Pages/MyBookings/MyBookings.js
@@ -1,11 +1,37 @@
-import { Container, Row, Button, Col } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Button,
+  Col,
+  Card,
+  ListGroupItem,
+  ListGroup,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import useBookings from "../../Hooks/useBookings";
-import Booking from "../Booking/Booking";
+import { MdMoney as Price, MdOutlineReviews as Review } from "react-icons/md";
+import { AiOutlineStar as Star } from "react-icons/ai";
+import { BsBookmarkCheck as Check } from "react-icons/bs";
+import { GrLocation as Destination, GrMapLocation } from "react-icons/gr";
 
 const MyBookings = () => {
   //const email = sessionStorage.getItem("email");
   const [bookings] = useBookings();
+  const cardImg = {
+    height: "200px",
+    width: "286px",
+  };
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/deleteBooking/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount) {
+          alert("Booking cancelled successfully.");
+        }
+      });
+  };
   return (
     <div>
       <Container>
@@ -25,7 +51,57 @@ const MyBookings = () => {
               </Col>
             ) : (
               bookings.map((booking) => (
-                <Booking key={booking._id} booking={booking}></Booking>
+                // <Booking key={booking._id} booking={booking}></Booking>
+                <Col lg={4} md={6} sm={12} key={booking._id}>
+                  <Card style={{ width: "18rem" }} className="mt-3 g-2 mx-auto">
+                    <Card.Img
+                      variant="top"
+                      src={booking?.image}
+                      style={cardImg}
+                      className="img-fluid"
+                    />
+                    <Card.Body>
+                      <Card.Title>
+                        <Destination
+                          className="me-1"
+                          size="1.5em"
+                        ></Destination>
+                        {booking?.destination}
+                      </Card.Title>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                      <ListGroupItem>
+                        {" "}
+                        <GrMapLocation className="me-1" size="1.5em" />
+                        {booking?.city}, {booking?.country}
+                      </ListGroupItem>
+                      <ListGroupItem>
+                        <Price className="me-1" size="1.5em" />${booking?.price}{" "}
+                        approx.
+                      </ListGroupItem>
+                      <ListGroupItem>
+                        <Star className="me-1" size="1.5em" />
+                        {booking?.rating}/5.0, {booking?.number_of_reviews}{" "}
+                        reviews.
+                      </ListGroupItem>
+                      <ListGroupItem>
+                        <Review className="me-1" size="1.5em" />
+                        {booking?.number_of_reviews} reviews.
+                      </ListGroupItem>
+                      <ListGroupItem>
+                        <div className="d-flex justify-content-center">
+                          <Button
+                            onClick={() => handleDelete(booking?._id)}
+                            variant="danger"
+                            className="text-light"
+                          >
+                            <Check /> Cancel Booking
+                          </Button>
+                        </div>
+                      </ListGroupItem>
+                    </ListGroup>
+                  </Card>
+                </Col>
               ))
             )}
           </Row>
